67ce0249e5c9c5f355a4886d71d9c292e77cf752
C-jimenez21 <cristianjjc21@gmail.com>
2023-08-16 11:24:41 -0500

    feat: :sparkles: Se establece la conexion con la base de datos

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,19 +1,21 @@
 import dotenv from "dotenv";
 import {MongoClient} from "mongodb"
-
-dotenv.config("../");
-
-export async function connection(){
+dotenv.config("../src/");
+export async function con(){
     try {
+        console.log(process.env.ATLAS_DATABASE);
         const URI = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.qbqr4gp.mongodb.net/${process.env.ATLAS_DATABASE}`;
+        //const URI =" mongodb+srv://C-jimenez21:admin21@cluster0.qbqr4gp.mongodb.net/DB_BODEGAS_V1"
         const options = {
             useNewUrlParser: true,
             useUnifiedTopology: true
         };
         const client = await MongoClient.connect(URI, options);
         return client.db()
-
     } catch (error) {
         return{status: 500, message: error};
     }
 }
+
+
+
